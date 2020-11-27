0b0bfdf8f26bc8fcebfb2199c06aa0c1f615f9f4
ayoub <ayo.daif@gmail.com>
2020-11-27 02:33:51 +0100

    include dtos in session controller

--- src/main/java/com/spring/tuto/conferencedemo/controllers/SessionController.java
+++ src/main/java/com/spring/tuto/conferencedemo/controllers/SessionController.java
@@ -1,12 +1,16 @@
 package com.spring.tuto.conferencedemo.controllers;
 
+import com.spring.tuto.conferencedemo.DTOs.SessionDTO;
+import com.spring.tuto.conferencedemo.mapper.SessionMapper;
 import com.spring.tuto.conferencedemo.models.Session;
 import com.spring.tuto.conferencedemo.repositories.SessionRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/sessions")
@@ -14,29 +18,34 @@ public class SessionController {
 
     @Autowired
     private SessionRepository sessionRepository;
+    @Autowired
+    private SessionMapper mapper;
 
     @GetMapping
-    public List<Session> list() {
-        return sessionRepository.findAll();
+    public List<SessionDTO> list() {
+        List<Session> sessions = sessionRepository.findAll();
+        return sessions.stream().map(x->mapper.toSessionDTO(x)).collect(Collectors.toList());
     }
 
      @GetMapping
      @RequestMapping("{id}")
-    public Session get(@PathVariable Long id){
-        return sessionRepository.getOne(id);
+    public SessionDTO get(@PathVariable Long id){
+        return mapper.toSessionDTO(sessionRepository.getOne(id));
  }
 
     @PostMapping
-    public Session create(@RequestBody final Session session){
-        return sessionRepository.saveAndFlush(session);
+    public SessionDTO create(@RequestBody final SessionDTO sessionDto){
+        Session sessionCreated = sessionRepository.saveAndFlush(mapper.toSession(sessionDto));
+        return (mapper.toSessionDTO(sessionCreated));
 
     }
 
     @RequestMapping(method= RequestMethod.PUT, value="{id}")
-    public Session update(@RequestBody Session session , @PathVariable Long id){
+    public SessionDTO update(@RequestBody SessionDTO sessionDto , @PathVariable Long id){
         Session existingSession = sessionRepository.getOne(id);
-        BeanUtils.copyProperties(session,existingSession, "session_id");
-        return sessionRepository.saveAndFlush(existingSession);
+        BeanUtils.copyProperties(mapper.toSession(sessionDto),existingSession, "session_id");
+       Session sessionUpdated = sessionRepository.saveAndFlush(existingSession);
+       return (mapper.toSessionDTO(sessionUpdated));
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value ="{id}")
--- src/main/java/com/spring/tuto/conferencedemo/repositories/SessionRepository.java
+++ src/main/java/com/spring/tuto/conferencedemo/repositories/SessionRepository.java
@@ -2,6 +2,8 @@ package com.spring.tuto.conferencedemo.repositories;
 
 import com.spring.tuto.conferencedemo.models.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SessionRepository extends JpaRepository<Session, Long> {
 }
